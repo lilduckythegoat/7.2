c93c2c6e2ee724b0fd641b52a3f683e18a41d7ed
lilduckythegoat <83048750+lilduckythegoat@users.noreply.github.com>
2021-08-28 01:51:33 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function change_sk8() {
+    window.location = "sk8.html";
+}
+
+function change_ps4() {
+    window.location = "ps4.html";
+}
+
+function change_apple() {
+    window.location = "apple.html";
+}
